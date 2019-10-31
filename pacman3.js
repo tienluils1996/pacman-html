33ef5e67ced85b91417cdaeac27ddae123928440
tienls1996 <tienluils01@gmail.com>
2019-10-31 12:24:50 +0700

    fill backgroumd-color

---
+++ pacman3.js
@@ -0,0 +1,97 @@
+var NONE        = 4,
+    UP          = 3,
+    LEFT        = 2,
+    DOWN        = 1,
+    RIGHT       = 11,
+    WAITING     = 5,
+    PAUSE       = 6,
+    PLAYING     = 7,
+    COUNTDOWN   = 8,
+    EATEN_PAUSE = 9,
+    DYING       = 10,
+    Pacman      = {};
+
+    var PACMAN = (function () {
+
+    var state        = WAITING,
+        audio        = null,
+        ghosts       = [],
+        ghostSpecs   = ["#00FFDE", "#FF0000", "#FFB8DE", "#FFB847"],
+        eatenCount   = 0,
+        level        = 0,
+        tick         = 0,
+        ghostPos, userPos, 
+        stateChanged = true,
+        timerStart   = null,
+        lastTime     = 0,
+        ctx          = null,
+        timer        = null,
+        map          = null,
+        user         = null,
+        stored       = null;
+
+Pacman.MAP = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+    [0, 4, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 4, 0],
+    [0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0],
+    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+    [0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0],
+    [0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0],
+    [0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0],
+    [2, 2, 2, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 2, 2, 2],
+    [0, 0, 0, 0, 1, 0, 1, 0, 0, 3, 0, 0, 1, 0, 1, 0, 0, 0, 0],
+    [2, 2, 2, 2, 1, 1, 1, 0, 3, 3, 3, 0, 1, 1, 1, 2, 2, 2, 2],
+    [0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
+    [2, 2, 2, 0, 1, 0, 1, 1, 1, 2, 1, 1, 1, 0, 1, 0, 2, 2, 2],
+    [0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+    [0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0],
+    [0, 4, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 4, 0],
+    [0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0],
+    [0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0],
+    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+];
+
+function init(wrapper, root) {
+        
+        var i, len, ghost,
+            blockSize = wrapper.offsetWidth / 19,
+            canvas    = document.createElement("canvas");
+        
+        canvas.setAttribute("width", (blockSize * 19) + "px");
+        canvas.setAttribute("height", (blockSize * 22) + 30 + "px");
+
+        wrapper.appendChild(canvas);
+        
+        ctx  = canvas.getContext('2d');
+        
+        function draw (ctx){
+            ctx.fillStyle = "#000"
+            ctx.fillRect(50, 50, 100,200);
+        }
+        draw(ctx);
+
+        
+        // map   = new Pacman.Map(blockSize);
+        // user  = new Pacman.User({ 
+        //     "completedLevel" : completedLevel, 
+        //     "eatenPill"      : eatenPill 
+        // }, map);
+
+        // for (i = 0, len = ghostSpecs.length; i < len; i += 1) {
+        //     ghost = new Pacman.Ghost({"getTick":getTick}, map, ghostSpecs[i]);
+        //     ghosts.push(ghost);
+        // }
+        
+        // map.draw(ctx);
+    };
+
+    
+    return {
+        "init" : init
+    };
+    
+}());
